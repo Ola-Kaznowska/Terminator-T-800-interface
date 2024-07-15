68cb74031617daf5c707e69e3b66d5dffd3b8ec4
Ola Kaznowska <118020449+Ola-Kaznowska@users.noreply.github.com>
2024-07-15 14:00:10 +0200

    Add files via upload

---
+++ Terminator T-800 interface.py
@@ -0,0 +1,19 @@
+scan = "SCAN LEVEL"
+stars = "*********"
+stars_left = "*****************"
+code = "0000000000"
+visual = "visual: "
+target = "TARGET"
+analysis = "ANALYSIS: MATCH:"
+vehi = "389 VEHI 55378" 
+
+
+
+print(f"{scan : <80}")
+print(f"{stars : <80}")
+print(f"{code : <80}")
+print(f"{visual : ^83}")
+print(f"{target : ^80}")
+print(f"{analysis : >100}")
+print(f"{stars_left : >100}")
+print(f"{vehi : >100}")
